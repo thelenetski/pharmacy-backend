67e92c0fdfa4a22db09d27de06fa180531328482
thelenetski <49438367+thelenetski@users.noreply.github.com>
2025-05-10 11:23:36 +0300

    fix cookie secure

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -15,10 +15,14 @@ export const loginUserController = async (req, res) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
     expires: new Date(Date.now() + ONE_DAY),
+    sameSite: 'None',
+    secure: true,
   });
   res.cookie('sessionId', session._id, {
     httpOnly: true,
     expires: new Date(Date.now() + ONE_DAY),
+    sameSite: 'None',
+    secure: true,
   });
 
   res.json({
@@ -69,10 +73,14 @@ const setupSession = (res, session) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
     expires: new Date(Date.now() + ONE_DAY),
+    sameSite: 'None',
+    secure: true,
   });
   res.cookie('sessionId', session._id, {
     httpOnly: true,
     expires: new Date(Date.now() + ONE_DAY),
+    sameSite: 'None',
+    secure: true,
   });
 };
 
