9f28ae3fae96d9b438a37fc269ae169cce7193e1
thelenetski <49438367+thelenetski@users.noreply.github.com>
2025-05-12 13:12:05 +0300

    add pagination to orders

--- src/controllers/orders.js
+++ src/controllers/orders.js
@@ -1,14 +1,22 @@
 import { ordersData } from '../services/orders.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const ordersController = async (req, res) => {
   const filter = parseFilterParams(req.query);
+  const { page, perPage } = parsePaginationParams(req.query);
 
-  const data = await ordersData({ filter });
+  const data = await ordersData({ page, perPage, filter });
 
   res.json({
     status: 200,
     message: 'Successfully get orders',
-    data,
+    page: data.page,
+    perPage: data.page,
+    totalItems: data.totalItems,
+    totalPages: data.totalPages,
+    hasNextPage: data.hasNextPage,
+    hasPreviousPage: data.hasPreviousPage,
+    data: data.orders,
   });
 };
--- src/services/orders.js
+++ src/services/orders.js
@@ -1,11 +1,25 @@
 import { ordersCollection } from '../db/models/orders.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const ordersData = async ({ filter = {} }) => {
-  const orders = ordersCollection.find(
-    filter.name && {
-      name: { $regex: filter.name, $options: 'i' },
-    },
-  );
+export const ordersData = async ({ page = 1, perPage = 5, filter = {} }) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
 
-  return orders;
+  const query = {};
+
+  if (filter.name) {
+    query.name = { $regex: filter.name, $options: 'i' };
+  }
+
+  const [total, orders] = await Promise.all([
+    ordersCollection.countDocuments(query),
+    ordersCollection.find(query).skip(skip).limit(limit),
+  ]);
+
+  const paginationData = calculatePaginationData(total, perPage, page);
+
+  return {
+    data: orders,
+    ...paginationData,
+  };
 };
