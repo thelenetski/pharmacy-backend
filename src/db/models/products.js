d804620c0ade0dad8f3e483c8092c082983ea7ca
thelenetski <49438367+thelenetski@users.noreply.github.com>
2025-06-06 16:29:36 +0300

    change model product

--- src/db/models/products.js
+++ src/db/models/products.js
@@ -2,12 +2,12 @@ import { model, Schema } from 'mongoose';
 
 const productsSchema = new Schema(
   {
-    photo: { type: String, required: true },
+    photo: { type: String },
     name: { type: String, required: true },
     suppliers: { type: String, required: true },
     stock: { type: String, required: true },
     price: { type: String, required: true },
-    category: { type: String, required: true },
+    category: { type: String },
   },
   { timestamps: false, versionKey: false },
 );
