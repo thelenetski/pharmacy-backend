c4b395453b1a619da5812ac75f8e21e5c8f6eb8a
thelenetski <49438367+thelenetski@users.noreply.github.com>
2025-06-30 20:06:04 +0300

    fix httpOnly to maxAge

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -14,13 +14,13 @@ export const loginUserController = async (req, res) => {
 
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
-    expires: new Date(Date.now() + ONE_WEEK),
+    maxAge: ONE_WEEK,
     sameSite: 'None',
     secure: true,
   });
   res.cookie('sessionId', session._id, {
     httpOnly: true,
-    expires: new Date(Date.now() + ONE_WEEK),
+    maxAge: ONE_WEEK,
     sameSite: 'None',
     secure: true,
   });
@@ -72,13 +72,13 @@ export const userInfoController = async (req, res) => {
 const setupSession = (res, session) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
-    expires: new Date(Date.now() + ONE_WEEK),
+    maxAge: ONE_WEEK,
     sameSite: 'None',
     secure: true,
   });
   res.cookie('sessionId', session._id, {
     httpOnly: true,
-    expires: new Date(Date.now() + ONE_WEEK),
+    maxAge: ONE_WEEK,
     sameSite: 'None',
     secure: true,
   });
